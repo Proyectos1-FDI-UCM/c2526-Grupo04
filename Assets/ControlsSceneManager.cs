d0dd9a5273ad0f6c73f9c43a29093170c7091f9f
jhoyos02 <jhoyos02@ucm.es>
2026-04-08 19:14:40 +0200

    Implementada escena de controles parcialmente
    Implementada escena de controles sin texto

---
+++ Assets/ControlsSceneManager.cs
@@ -0,0 +1,131 @@
+//---------------------------------------------------------
+// Breve descripción del contenido del archivo
+// Responsable de la creación de este archivo
+// Nombre del juego
+// Proyectos 1 - Curso 2025-26
+//---------------------------------------------------------
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+// Añadir aquí el resto de directivas using
+
+
+/// <summary>
+/// Antes de cada class, descripción de qué es y para qué sirve,
+/// usando todas las líneas que sean necesarias.
+/// </summary>
+public class ControlsSceneManager : MonoBehaviour
+{
+    // ---- ATRIBUTOS DEL INSPECTOR ----
+    #region Atributos del Inspector (serialized fields)
+    // Documentar cada atributo que aparece aquí.
+    // El convenio de nombres de Unity recomienda que los atributos
+    // públicos y de inspector se nombren en formato PascalCase
+    // (palabras con primera letra mayúscula, incluida la primera letra)
+    // Ejemplo: MaxHealthPoints
+
+    [SerializeField] private GameObject GamePadControls;
+    [SerializeField] private GameObject KeyboardControls;
+    [SerializeField] private GameObject GamePadControlsOut;
+    [SerializeField] private GameObject KeyboardControlsOut;
+
+    #endregion
+
+    // ---- ATRIBUTOS PRIVADOS ----
+    #region Atributos Privados (private fields)
+    // Documentar cada atributo que aparece aquí.
+    // El convenio de nombres de Unity recomienda que los atributos
+    // privados se nombren en formato _camelCase (comienza con _, 
+    // primera palabra en minúsculas y el resto con la 
+    // primera letra en mayúsculas)
+    // Ejemplo: _maxHealthPoints
+
+    private static ControlsSceneManager _instance;
+
+    #endregion
+    
+    // ---- MÉTODOS DE MONOBEHAVIOUR ----
+    #region Métodos de MonoBehaviour
+    
+    // Por defecto están los típicos (Update y Start) pero:
+    // - Hay que añadir todos los que sean necesarios
+    // - Hay que borrar los que no se usen 
+    
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before 
+    /// any of the Update methods are called the first time.
+    /// </summary>
+    void Start()
+    {
+        
+    }
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        
+    }
+    #endregion
+
+    // ---- MÉTODOS PÚBLICOS ----
+    #region Métodos públicos
+    // Documentar cada método que aparece aquí con ///<summary>
+    // El convenio de nombres de Unity recomienda que estos métodos
+    // se nombren en formato PascalCase (palabras con primera letra
+    // mayúscula, incluida la primera letra)
+    // Ejemplo: GetPlayerController
+
+    /// <summary>
+    /// Propiedad para acceder a la única instancia de la clase.
+    /// </summary>
+    public static ControlsSceneManager Instance
+    {
+        get
+        {
+            Debug.Assert(_instance != null);
+            return _instance;
+        }
+    }
+
+    /// <summary>
+    /// Devuelve cierto si la instancia del singleton está creada y
+    /// falso en otro caso.
+    /// Lo normal es que esté creada, pero puede ser útil durante el
+    /// cierre para evitar usar el ControlsSceneManager que podría haber sido
+    /// destruído antes de tiempo.
+    /// </summary>
+    /// <returns>Cierto si hay instancia creada.</returns>
+    public static bool HasInstance()
+    {
+        return _instance != null;
+    }
+
+
+    public void GamePadControlsHUD(bool ver)
+    {
+        GamePadControls.SetActive(ver);
+        EventSystem.current.SetSelectedGameObject(GamePadControls.GetComponentInChildren<Button>().gameObject);
+    }
+
+    public void KeyboardControlsHUD(bool ver)
+    {
+        KeyboardControls.SetActive(ver);
+        EventSystem.current.SetSelectedGameObject(KeyboardControls.GetComponentInChildren<Button>().gameObject);
+    }
+
+    #endregion
+
+    // ---- MÉTODOS PRIVADOS ----
+    #region Métodos Privados
+    // Documentar cada método que aparece aquí
+    // El convenio de nombres de Unity recomienda que estos métodos
+    // se nombren en formato PascalCase (palabras con primera letra
+    // mayúscula, incluida la primera letra)
+
+    #endregion
+
+} // class ControlsSceneManager 
+// namespace
